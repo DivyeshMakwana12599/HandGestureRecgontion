d6b86cdf533e6c0bd01bd6993ec57540d33d206f
Divyesh Makwana <divyeshmakwana12599@gmail.com>
2021-04-27 22:24:07 +0530

    Hands-Pipe and FPS Displayed on img

--- HandTrackingMin.py
+++ HandTrackingMin.py
@@ -5,18 +5,26 @@ import time
 running = True
 cap = cv2.VideoCapture(0)
 
-myHands = mp.solutions.hands
-hands = myHands.Hands()
-
-
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+cTime = pTime = 0
 
 
 while running:
     sucess , img = cap.read()
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results =  hands.process(imgRGB)
-    print(results)
+    # print(results.multi_hand_landmarks)
 
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+    cv2.putText(img, str(int(fps)),(0,50), cv2.FONT_HERSHEY_COMPLEX, 2, (255,0,0), 2)
     cv2.imshow('Image', img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         running = False
